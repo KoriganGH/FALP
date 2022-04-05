2e7203c3984edabd5bda8156e18a2b29ee993d2f
KoriganGH <96747698+KoriganGH@users.noreply.github.com>
2022-04-05 11:26:34 +0300

    Add files via upload

---
+++ lab_5/n_11/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let question m =
+  match m with
+   |"F#"->printf "Ты подлиза!"
+   |"Prolog"->printf "Ты подлиза!"
+   |_->printf "Все понятно..."
+   
+[<EntryPoint>]
+let main argv =
+    printfn "Какой твой любимый язык программирования?"
+    let m = System.Console.ReadLine();
+    question m
+    0
