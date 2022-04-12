728f939755a825c19edbdc7a299a59f8c9f80710
KoriganGH <96747698+KoriganGH@users.noreply.github.com>
2022-04-12 11:06:14 +0300

    Add files via upload

---
+++ lab_5/n_18/n_18_2/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let m2 x =
+    let rec obhod x c=
+        if x <= 0 then c else
+        if ((x % 10) > 3) && ((x % 10) % 2 > 0) then obhod (x / 10) (c + 1)
+        else obhod (x / 10) c
+    obhod x 0
+
+[<EntryPoint>]
+let main argv =
+    let x = System.Convert.ToInt32(System.Console.ReadLine())
+    System.Console.WriteLine(m2 x)
+    0
