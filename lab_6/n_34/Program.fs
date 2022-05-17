302cfd717ade7dc8df74fc11fdbea8903b9bbde5
KoriganGH <96747698+KoriganGH@users.noreply.github.com>
2022-05-17 09:04:50 +0300

    Add files via upload

---
+++ lab_6/n_34/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let rec input_list n=
+    if n = 0 then []
+    else 
+        let head = Convert.ToInt32(Console.ReadLine())
+        let tail = input_list (n - 1)
+        head::tail
+
+let rec count f list c =
+    match list with 
+    |h::t -> if f h t then count f t (c + 1) else count f t c
+    |[] -> c
+
+let task list =
+    let a = Convert.ToInt32(Console.ReadLine())
+    let b = Convert.ToInt32(Console.ReadLine())
+    count (fun x y -> (x < b) && (x > a)) list 0  
+
+[<EntryPoint>]
+let main argv =
+    let n = Convert.ToInt32(Console.ReadLine())
+    let list = input_list n
+    list |> task |> Console.WriteLine
+    0
