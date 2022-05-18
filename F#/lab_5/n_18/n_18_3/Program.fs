f89db5895382a405b499207caa1cedc8812d8ce0
KoriganGH <96747698+KoriganGH@users.noreply.github.com>
2022-05-18 22:26:11 +0300

    Add files via upload

---
+++ F#/lab_5/n_18/n_18_3/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let sum x=
+    let rec sum1 c x=
+        if x = 0 then c
+        else sum1 (c + (x % 10)) (x / 10)
+    sum1 0 x
+
+let rec m3 x y c=
+    if x < y then c 
+        else 
+        if (x % y = 0) && (sum x > sum y) then m3 x (y+1) (c * y) 
+        else m3 x (y+1) c
+
+[<EntryPoint>]
+let main argv =
+    let x = System.Convert.ToInt32(System.Console.ReadLine())
+    System.Console.WriteLine(m3 x 1 1);
+    0
