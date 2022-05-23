29ba3e727f335265fb4ce47752ba612e48fa7446
Kirill <96747698+KoriganGH@users.noreply.github.com>
2022-05-23 15:46:22 +0300

    Update lab_12.pl

--- SWI-Prolog/lab_12.pl
+++ SWI-Prolog/lab_12.pl
@@ -7,3 +7,7 @@ prime(X,Y):- Y1 is Y-1, prime(X,Y1).
 sprime_up(X,Sum):-sprime_up(X,X,Sum),!.
 sprime_up(_,0,0):-!.
 sprime_up(X,Del,Sum):-D1 is Del-1, sprime_up(X,D1,NewSum), (0 is X mod Del,prime(Del), Sum is NewSum+Del; Sum is NewSum).
+
+sprime_down(X,Sum):-sprime_down(X,X,Sum,0),!.
+sprime_down(_,0,Sum,Sum):-!.
+sprime_down(X,Del,Sum,CurSum):-(0 is X mod Del,prime(Del), NewSum is CurSum+Del; NewSum is CurSum),D1 is Del-1, sprime_down(X,D1,Sum,NewSum).
