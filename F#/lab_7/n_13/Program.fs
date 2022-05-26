0e2ce1133a9443e1a84ab496bdb5afe177898964
Kirill <96747698+KoriganGH@users.noreply.github.com>
2022-05-26 19:51:45 +0300

    Add files via upload

---
+++ F#/lab_7/n_13/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let rec input_list n=
+    if n = 0 then []
+    else 
+        let head = Convert.ToInt32(Console.ReadLine())
+        let tail = input_list (n - 1)
+        head::tail
+
+let rec output_list (list:int list) =
+    match list with
+    |[] -> None
+    |h::t-> Console.Write("{0} ", h)
+            output_list t
+
+let task27 (list:int list) =
+    (List.last list::List.removeAt (List.length list - 1) list)
+
+[<EntryPoint>]
+let main argv =
+    let list = input_list (Convert.ToInt32(Console.ReadLine()))
+    list |> task27 |> output_list |> ignore
+    0
