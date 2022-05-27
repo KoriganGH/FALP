c169dcfa6dbe469620d5aa5505f840129b8efe2f
Kirill <96747698+KoriganGH@users.noreply.github.com>
2022-05-27 09:24:54 +0300

    Add files via upload

---
+++ F#/lab_7/n_19/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let task1 str = 
+    let res1 = String.length(String.filter (fun x -> x>='А' && x<='я') str)
+    Console.WriteLine(res1)
+
+let f n str  = 
+    match n with 
+    |1 -> Convert.ToString(task1 str)
+    
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите строку: ")
+    let str = Console.ReadLine()
+    Console.WriteLine("Введите номер задания: ")
+    let n = Console.ReadLine() |> Convert.ToInt32
+    f n str |> ignore 
+    0
