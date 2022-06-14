b312e1bfb43d086d8c3a69c4641afc1c8fff9f9a
Kirill <96747698+KoriganGH@users.noreply.github.com>
2022-06-14 14:49:45 +0300

    Add files via upload

---
+++ F#/lab_8/n_4/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let printerAgent = MailboxProcessor.Start(fun inbox->
+    let rec messageLoop() = async{
+        let! msg = inbox.Receive()
+        match msg with
+        | "F#" -> printfn "%s"("Ты подлиза!")
+        | "Prolog" -> printfn "%s"("Ты подлиза!")
+        | _ -> printfn "%s"("Все понятно..")
+        return! messageLoop()
+        }
+    messageLoop()
+    )
+
+[<EntryPoint>]
+let main argv =
+    printerAgent.Post("F#")
+    printerAgent.Post("Prolog")
+    printerAgent.Post("Python")
+    Console.ReadKey() |> ignore
+    0 
