2a26086cd88c62b3d6d8a2e13dcb322cc3c2a256
Kirill <96747698+KoriganGH@users.noreply.github.com>
2022-06-14 20:38:32 +0300

    8

--- F#/lab_8/n_5-10/Program.fs
+++ F#/lab_8/n_5-10/Program.fs
@@ -2,12 +2,96 @@ open System
 open System.Text.RegularExpressions
 open System.Diagnostics
  
+let regSeries (str:string) =
+    let r = @"^([0-9]{2}[0-9]{2})?$"
+    Regex.IsMatch(str,r)
+
+let regNumber (str:string) =
+    let r = @"^([0-9]{6})?$"
+    Regex.IsMatch(str,r)
+
+let regBirthDate (str:string) =
+    let r = @"^(0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](19|20)\d\d$"
+    Regex.IsMatch(str,r)
+
+let regFullName (str:string) =
+    let r = @"^([А-ЯA-Z]|[А-ЯA-Z][\x27а-яa-z]{1,}|[А-ЯA-Z][\x27а-яa-z]{1,}\-([А-ЯA-Z][\x27а-яa-z]{1,}|(оглы)|(кызы)))\040[А-ЯA-Z][\x27а-яa-z]{1,}(\040[А-ЯA-Z][\x27а-яa-z]{1,})?$"
+    Regex.IsMatch(str,r)
+
+let regGender(str:string) =
+    let r = "(^Муж$)|(^муж$)|(^Жен$)|(^жен$)|(^Мужской$)|(^мужской$)|(^Женский$)|(^женский$)|(^М$)|(^м$)|(^Ж$)|(^ж$)"
+    Regex.IsMatch(str,r)
+
 type PassportRF(_series,_number,_fullName,_gender,_birthdate) =
-    member val Series = _series 
-    member val Number = _number
-    member val FullName = _fullName
-    member val Gender = _gender
-    member val BirthDate = _birthdate      
+    member val Series = 
+        if regSeries _series then _series
+        else
+            let rec prom str t_f =
+                match str,t_f with
+                |other,true -> other
+                |other,false ->
+                    Console.Clear()
+                    Console.WriteLine("Неверный формат серии паспорта! Введите корректный вариант:")
+                    let s = Console.ReadLine()
+                    let new_t_f = if regSeries s then true else false
+                    prom s new_t_f
+            prom "" false 
+            
+    member val Number = 
+        if regNumber _number then _number
+        else
+            let rec prom str t_f =
+                match str,t_f with
+                |other,true -> other
+                |other,false ->
+                    Console.Clear()
+                    Console.WriteLine("Неверный формат номера паспорта! Введите корректный вариант:")
+                    let s = Console.ReadLine()
+                    let new_t_f = if regNumber s then true else false
+                    prom s new_t_f
+            prom "" false  
+
+    member val FullName = 
+        if regFullName _fullName then _fullName
+        else
+            let rec prom str t_f =
+                match str,t_f with
+                |other,true -> other
+                |other,false ->
+                    Console.Clear()
+                    Console.WriteLine("Неверный формат ФИО! Введите корректный вариант:")
+                    let s = Console.ReadLine()
+                    let new_t_f = if regFullName s then true else false
+                    prom s new_t_f
+            prom "" false  
+       
+    member val Gender =
+        if regGender _gender then _gender
+        else
+            let rec prom str t_f =
+                match str,t_f with
+                |other,true -> other
+                |other,false ->
+                    Console.Clear()
+                    Console.WriteLine("Неверный формат пола! Введите корректный вариант:")
+                    let s = Console.ReadLine()
+                    let new_t_f = if regGender s then true else false
+                    prom s new_t_f
+            prom "" false  
+       
+    member val BirthDate =
+        if regBirthDate _birthdate then _birthdate
+        else 
+            let rec prom str t_f =
+                match str,t_f with
+                |other,true -> other
+                |other,false ->
+                    Console.Clear()
+                    Console.WriteLine("Неверный формат даты рождения! Введите корректный вариант:")
+                    let s = Console.ReadLine()
+                    let new_t_f = if regBirthDate s then true else false
+                    prom s new_t_f
+            prom "" false  
 
     override this.ToString() = 
         sprintf "Series: %s | Number: %s | FullName: %s | Gender: %s | BirthDate: %s" this.Series this.Number this.FullName this.Gender this.BirthDate
