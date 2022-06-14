5a212b17da7bb37bb784c04d9fe5092d8a2c23ab
Kirill <96747698+KoriganGH@users.noreply.github.com>
2022-06-14 15:50:06 +0300

    Add files via upload

---
+++ F#/lab_8/n_3/Program.fs
@@ -0,0 +1,55 @@
+﻿open System
+open FParsec
+
+type Expr = 
+ | Num of float
+ | Plus of Expr * Expr
+ | Minus of Expr * Expr
+
+let pstring_ws s = spaces >>. pstring s .>> spaces
+let float_ws = spaces >>. pfloat .>> spaces
+let parseExpression, implementation =  createParserForwardedToRef<Expr, unit>()
+let parsePlus = tuple2 (parseExpression .>> pstring_ws "+") parseExpression |>> Plus
+let parseMinus = tuple2 (parseExpression .>> pstring_ws "-") parseExpression |>> Minus
+let parseOp = between (pstring_ws "(") (pstring_ws ")") (attempt parsePlus <|> parseMinus)
+let parseNum = float_ws |>> Num 
+implementation := parseNum <|> parseOp
+
+let rec EvalExpr(e:Expr):float = 
+ match e with
+ | Num(num) -> num
+ | Plus(a,b) ->
+    let left = 
+        match a with
+        | Num(num) -> num
+        | _ -> EvalExpr(a)
+    let right = 
+        match b with
+        | Num(num) -> num
+        | _ -> EvalExpr(b)
+    let res1 = left + right
+    printfn "%f + %f = %f" left right res1 
+    res1
+ | Minus(a,b) ->
+    let left = 
+        match a with
+        | Num(num) -> num
+        | _ -> EvalExpr(a)
+    let right = 
+        match b with
+        | Num(num) -> num
+        | _ -> EvalExpr(b)
+    let res2 = left - right
+    printfn "%f - %f = %f" left right res2 
+    res2
+
+[<EntryPoint>]
+let main argv =
+    let expr1Parser = run parseExpression "(((3+4)+(1-2))+10)"
+    printfn "%A" expr1Parser
+    match expr1Parser with
+    | Success(result, _, _) ->
+        let eval1 = EvalExpr(result)
+        printfn "Результат вычислений: %f" eval1
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+    0 
