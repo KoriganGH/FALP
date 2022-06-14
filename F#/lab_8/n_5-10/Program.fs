618cb329e8aa2cb1e585e5ac483440659dc225f9
Kirill <96747698+KoriganGH@users.noreply.github.com>
2022-06-14 18:20:45 +0300

    5

---
+++ F#/lab_8/n_5-10/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.Text.RegularExpressions
+open System.Diagnostics
+    
+type PassportRF(_series,_number,_fullName,_gender,_birthdate) =
+    member val Series = _series 
+    member val Number = _number
+    member val FullName = _fullName
+    member val Gender = _gender
+    member val BirthDate = _birthdate      
+
+[<EntryPoint>]
+let main argv =
+
+    0 
