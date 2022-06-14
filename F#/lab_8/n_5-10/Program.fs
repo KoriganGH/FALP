56cdd4270320ca8e6552c2c6f2157c91749ce095
Kirill <96747698+KoriganGH@users.noreply.github.com>
2022-06-14 18:37:42 +0300

    6

--- F#/lab_8/n_5-10/Program.fs
+++ F#/lab_8/n_5-10/Program.fs
@@ -1,7 +1,7 @@
-﻿open System
+open System
 open System.Text.RegularExpressions
 open System.Diagnostics
-    
+ 
 type PassportRF(_series,_number,_fullName,_gender,_birthdate) =
     member val Series = _series 
     member val Number = _number
@@ -9,7 +9,11 @@ type PassportRF(_series,_number,_fullName,_gender,_birthdate) =
     member val Gender = _gender
     member val BirthDate = _birthdate      
 
+    override this.ToString() = 
+        sprintf "Series: %s | Number: %s | FullName: %s | Gender: %s | BirthDate: %s" this.Series this.Number this.FullName this.Gender this.BirthDate
+
 [<EntryPoint>]
 let main argv =
-
+    let test = PassportRF("0312","123456","Александр Первый","муж","25.12.1984")
+    printfn "%O" (test.ToString())
     0 
